ff8848034570cc089553dac44306ef00ec3c99ce
rory-linehan <2692329+rory-linehan@users.noreply.github.com>
2019-10-17 11:22:55 -0500

    Create steup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+# Copyright 2019 Rory Linehan
+# this code is distributed under the terms of the GNU General Public License
+
+from distutils.core import setup
+
+setup(
+    name='pytprint',
+    packages=['pytprint',],
+    license='GNU GPL',
+    long_description=open('README.md').read(),
+)
