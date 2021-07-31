3a322be14fe6ff51b641eb0234993ad2f0469c82
Rory Linehan <2692329+rory-linehan@users.noreply.github.com>
2021-07-30 21:36:21 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from distutils.core import setup
 
 setup(
     name='pytprint',
-    version='0.2.1',
+    version='0.2.2',
     packages=['pytprint',],
     license='GNU GPL',
     long_description=open('README.md').read(),
